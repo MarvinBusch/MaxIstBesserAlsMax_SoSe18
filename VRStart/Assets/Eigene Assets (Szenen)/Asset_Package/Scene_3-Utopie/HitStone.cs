0731909f157e6cabf8622c744198783e53bdec9b
Marvin Busch <marvin.busch@live.de>
2018-06-13 15:26:59 +0200

    Interrogation Neues Licht

---
+++ VRStart/Assets/Eigene Assets (Szenen)/Asset_Package/Scene_3-Utopie/HitStone.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HitStone : MonoBehaviour {
+	public string scenename = "Limbus";
+
+	void OnTriggerEnter(Collider other){
+		SaveVariable.SceneChange(scenename);
+	}
+}
