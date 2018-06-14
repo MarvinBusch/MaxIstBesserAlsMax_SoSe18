7462bd002c42f0686a2782cb40b4413b3ba9d08b
unknown <marvin.busch@live.de>
2018-06-15 01:34:56 +0200

    Blade Runner / Erste Interaktion (Erste Kruzung Fertig)

---
+++ VRStart/Assets/Eigene Assets (Szenen)/Asset_Package/Szene_3-Dystopie/Neue Assets/FarbwechselPfeile.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FarbwechselPfeile : MonoBehaviour {
+
+	public Material notactive;
+	public Material active;
+	public Material lookat;
+
+	// Use this for initialization
+	void Start () {
+		Deaktivieren ();
+	}
+		
+	public void Aktivieren()
+	{
+		GetComponent<MeshCollider> ().enabled = true;
+		GetComponent<Renderer>().material = active;
+	}
+
+	public void Deaktivieren()
+	{
+		GetComponent<MeshCollider> ().enabled = false;
+		GetComponent<Renderer>().material = notactive;
+	}
+
+	public void TotallyWatching()
+	{
+		GetComponent<Renderer>().material = lookat;
+	}
+
+	public void NotEvenLooking()
+	{
+		GetComponent<Renderer>().material = active;
+	}
+}
---
+++ VRStart/Assets/Eigene Assets (Szenen)/Asset_Package/Szene_3-Dystopie/Neue Assets/HitDecision.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HitDecision : MonoBehaviour {
+
+	public int Decision;
+	bool Ansehen = false;
+	bool Links = false;
+	bool Mitte = false;
+	bool Rechts = false;
+	float Mytime = 0f;
+	public float Duration;
+
+	public GameObject Raumschiff;
+
+	public GameObject Schrift_de_1;
+
+	public GameObject Pfeil_li;
+	public GameObject Pfeil_mi;
+	public GameObject Pfeil_re;
+
+	void Start(){
+		Schrift_de_1.SetActive(false);
+		PfeilWirdNichtAngesehen ();
+		Raumschiff.GetComponent<Animator> ().SetFloat ("PlayMulti", 1f);
+	}
+
+	void OnTriggerEnter(Collider other){
+		Pfeil_li.GetComponent<FarbwechselPfeile> ().Aktivieren ();
+		Pfeil_mi.GetComponent<FarbwechselPfeile> ().Aktivieren ();
+		Pfeil_re.GetComponent<FarbwechselPfeile> ().Aktivieren ();
+
+		Schrift_de_1.SetActive(true);
+		Raumschiff.GetComponent<Animator>().SetFloat("PlayMulti", 0f);
+	}
+
+	void OnTriggerExit(Collider other){
+		Pfeil_li.GetComponent<FarbwechselPfeile> ().Deaktivieren ();
+		Pfeil_mi.GetComponent<FarbwechselPfeile> ().Deaktivieren ();
+		Pfeil_re.GetComponent<FarbwechselPfeile> ().Deaktivieren ();
+		Raumschiff.GetComponent<Animator> ().SetInteger ("State", 0);
+		Schrift_de_1.SetActive(false);
+	}
+
+	void Update(){
+		if (Ansehen == true) {
+			Mytime += Time.deltaTime;
+
+			if (Mytime > Duration) {
+				
+				Pfeil_li.GetComponent<FarbwechselPfeile> ().Deaktivieren ();
+				Pfeil_mi.GetComponent<FarbwechselPfeile> ().Deaktivieren ();
+				Pfeil_re.GetComponent<FarbwechselPfeile> ().Deaktivieren ();
+
+				if(Links==true){Raumschiff.GetComponent<Animator> ().SetInteger ("State",1);Pfeil_li.GetComponent<FarbwechselPfeile> ().TotallyWatching ();}
+				if(Mitte==true){Raumschiff.GetComponent<Animator> ().SetInteger ("State",2);Pfeil_mi.GetComponent<FarbwechselPfeile> ().TotallyWatching ();}
+				if(Rechts==true){Raumschiff.GetComponent<Animator> ().SetInteger ("State",3);Pfeil_re.GetComponent<FarbwechselPfeile> ().TotallyWatching ();}
+				Raumschiff.GetComponent<Animator> ().SetFloat ("PlayMulti", 1f);
+				PfeilWirdNichtAngesehen ();
+			}
+		}
+	}
+
+	public void LinksWirdAngesehen(){
+		Ansehen = true;
+		Links = true;
+	}
+
+	public void MitteWirdAngesehen(){
+		Ansehen = true;
+		Mitte = true;
+	}
+
+	public void RechtsWirdAngesehen(){
+		Ansehen = true;
+		Rechts = true;
+	}
+
+	public void PfeilWirdNichtAngesehen(){
+		Ansehen = false;
+		Links = false;
+		Mitte = false;
+		Rechts = false;
+		Mytime = 0f;
+	}
+}
