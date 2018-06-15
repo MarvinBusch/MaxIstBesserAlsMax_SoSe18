ffa453b15f43d80d0bf85193bd4de09b7aaf1218
unknown <marvin.busch@live.de>
2018-06-15 02:39:19 +0200

    Limbus Zeit Zählen eingepflegt

--- VRStart/Assets/Eigene Assets (Szenen)/Asset_Package/SaveVariable.cs
+++ VRStart/Assets/Eigene Assets (Szenen)/Asset_Package/SaveVariable.cs
@@ -6,15 +6,21 @@ using UnityEngine.SceneManagement;
 public class SaveVariable : MonoBehaviour {
 
 	static public bool Utopie;
+	static public float Zeit_Seit_Start = 0f;
 	static public string letzteSzene = "";
 	static public string aktuelleSzene = "";
 	
 	void Start(){
 		Utopie = true;
 		letzteSzene = "";
-		aktuelleSzene = "Start";
+		aktuelleSzene = "Start_zug";
+		Zeit_Seit_Start = 0f;
 	}
-	
+
+	void Udate(){
+		Zeit_Seit_Start += Time.deltaTime;
+	}
+
 	public void SetUtopieTrue(){Utopie=true;}
 	public void SetUtopieFalse(){Utopie=false;}
 
---
+++ VRStart/Assets/Eigene Assets (Szenen)/Asset_Package/Scene_5-Limbus/DisplayTime.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DisplayTime : MonoBehaviour {
+
+	float myTime = 0f;
+
+	void Start () {
+		myTime = 0f;
+		changeDisplayTime ();
+	}
+
+	void Update () {
+		if (SaveVariable.Zeit_Seit_Start > 0) {myTime = SaveVariable.Zeit_Seit_Start;} 
+		else {myTime += Time.deltaTime;}
+
+		changeDisplayTime ();
+	}
+
+	void changeDisplayTime(){
+		GetComponent<TextMesh> ().text = (Mathf.Floor(myTime/60)).ToString("F0") + ":" + (myTime%60).ToString("F1");
+	}
+}
